a83e4324ca139383eb6c416f6bc7ccd805a8e95f
young <young@howoocast.com>
2021-04-21 17:32:44 +0900

    ControllerAdvice - ExceptionHandler 이용 시 예제

---
+++ src/main/java/com/howoocast/ErrorRestController.java
@@ -0,0 +1,34 @@
+package com.howoocast;
+
+import com.howoocast.demo.exception.DataNotFoundException;
+import com.howoocast.demo.exception.EmptyValueException;
+import com.howoocast.demo.exception.UniqueViolationException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestControllerAdvice("com.howoocast.demo")
+@Slf4j
+public class ErrorRestController {
+
+	@ExceptionHandler(UniqueViolationException.class)
+	public ResponseEntity<?> uniqueViolationException(UniqueViolationException e) {
+		log.debug("[Error Rest Controller] {}", e.getClass().getSimpleName());
+		return e.getResponse();
+	}
+
+	@ExceptionHandler(EmptyValueException.class)
+	public ResponseEntity<?> emptyValueException(EmptyValueException e) {
+		log.debug("[Error Rest Controller] {}", e.getClass().getSimpleName());
+		return e.getResponse();
+	}
+
+	@ExceptionHandler(DataNotFoundException.class)
+	public ResponseEntity<?> dataNotFoundException(DataNotFoundException e) {
+		log.debug("[Error Rest Controller] {}", e.getClass().getSimpleName());
+		return e.getResponse();
+	}
+}
